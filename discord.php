ab88f7d2154ccd3ae53f9e29a5b06932bfc0a8aa
Unknown <airbonestheoriginal@gmail.com>
2018-10-12 16:37:36 +0200

    retest some code

--- discord.php
+++ discord.php
@@ -1,31 +1,18 @@
 <?php
-include __DIR__.'/vendor/autoload.php';
-use Discord\Discord;
-use Discord\Voice\VoiceClient;
-use Discord\Parts\Channel\Channel;
-use Discord\Parts\User\Game;
-use Discord\Parts\Embed;
-use Discord\Factory\Factory;
-$token='NTAwMzA0OTk0MzQ4NTY0NTI3.DqJAxA.VSorQuPbhnfdJn5tz5eIrra6i_0';
-$bot_id='500304994348564527';
 
-$discord = new Discord([
-    'token' => $token,
+include __DIR__.'/vendor/autoload.php';
+
+$discord = new \Discord\Discord([
+    'token' => 'NTAwMzA0OTk0MzQ4NTY0NTI3.DqJAxA.VSorQuPbhnfdJn5tz5eIrra6i_0',
 ]);
 
 $discord->on('ready', function ($discord) {
-    $game = $discord->factory(Game::class, ['name' => 'faire le tuto !',]);
-    $discord->updatePresence($game);
-    $discord->on('message', function ($message, $discord) {
-        echo "message received from: ".$message->author->username.$message->author->id.' msg id:'.$message->id.' msg:'.$message->content . ' channel id:' . $message->channel_id, PHP_EOL;
+    echo "Bot is ready.", PHP_EOL;
 
-        $tab_bonjour = array("Bonjour", "Hello", "Salut");
-        $randombonjour=rand(0,count($tab_bonjour)-1);
-        if($message->author->id != $bot_id && strstr($message->content, 'onjour')) // message pas du bot + contient le mot "bonjour"
-        {
-            $reponse=$tab_bonjour[$randombonjour]; // on crée la réponse
-            $message->channel->sendMessage($reponse); //on l'envoie dans le même channel
-        }
+    // Listen for events here
+    $discord->on('message', function ($message) {
+        echo "Recieved a message from {$message->author->username}: {$message->content}", PHP_EOL;
     });
 });
+
 $discord->run();
